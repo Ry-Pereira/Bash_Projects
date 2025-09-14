fe93226b90cd5884a290dcaf110b80854aaae3c3
Ryan <ryantonypereira@gmail.com>
2025-09-13 19:03:06 -0500

    Create user_greeting.sh
    Created the user greeting bash shell script program.

---
+++ greet_user/user_greeting.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Bash systsem echos or texts the message "Hi There" to the command line
+echo "Hi There"
+#Sleeps to 2 seconds, or the system command waits for two seconds before proceding to the nex line in the Bash code
+sleep 2
+#Bash systsem echos or texts the message "Welcome To" to the command line
+echo "Welcome To"
+#Sleeps to 2 seconds, or the system command waits for two seconds before proceding to the nex line in the Bash code
+sleep 2
+#Bash systsem echos or texts the message "BASH" to the command line
+echo "BASH"
+
