d8946856d55a6bc285d8c6e9d414057e32ba585e
Ryan <ryantonypereira@gmail.com>
2025-09-18 11:31:11 -0500

    Create number_counter.sh
    Added Code

---
+++ numbe_ counter/number_counter.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Welcome to the Number Counter Program!!!"
+sleep 2
+
+echo "Number to Count To:"
+read number_to_count_to
+echo " "
+sleep 2
+
+
+number=0
+until [[ $number -gt $number_to_count_to ]]
+do
+    echo "$number"
+    sleep 1
+done
+
+echo " "
+echo " "
+echo "Done"
+echo "Exiting"
+exit 1
